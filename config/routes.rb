062440447b6793cd5a10ca55cbe3b95446baa8cc
Marcelo Lauxen <marcelolauxen16@gmail.com>
2022-11-03 08:16:55 -0300

    add sidekiq-scheduler UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
